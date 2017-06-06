48599e25d7ea119a7b89fcce1d2a042f0eae5d9d
Andy Babic <ababic@rkh.co.uk>
2017-06-06 09:41:18 +0100

    Update versions in deprecation warning names… these will be live in `2.3`, so the earliest something can be deprecated is `2.4`

--- wagtailmenus/utils/deprecation.py
+++ wagtailmenus/utils/deprecation.py
@@ -1,12 +1,12 @@
 from __future__ import absolute_import, unicode_literals
 
 
-class RemovedInWagtailMenus23Warning(DeprecationWarning):
+class RemovedInWagtailMenus24Warning(DeprecationWarning):
     pass
 
 
-removed_in_next_version_warning = RemovedInWagtailMenus23Warning
+removed_in_next_version_warning = RemovedInWagtailMenus24Warning
 
 
-class RemovedInWagtailMenus24Warning(PendingDeprecationWarning):
+class RemovedInWagtailMenus25Warning(PendingDeprecationWarning):
     pass
