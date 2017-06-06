aa69ef4162ea64a0cc6674a92bc71c475c5785a9
Andy Babic <ababic@rkh.co.uk>
2017-06-06 08:13:07 +0100

    Add some wagtailmenus-specific deprecation warnings for use within the project

---
+++ wagtailmenus/utils/deprecation.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import, unicode_literals
+
+
+class RemovedInWagtailMenus23Warning(DeprecationWarning):
+    pass
+
+
+removed_in_next_version_warning = RemovedInWagtailMenus23Warning
+
+
+class RemovedInWagtailMenus24Warning(PendingDeprecationWarning):
+    pass
