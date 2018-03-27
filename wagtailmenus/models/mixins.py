e2462c8847aea594f91559b75512e3b2da286ddc
Andy Babic <ababic@rkh.co.uk>
2018-03-27 12:57:38 +0100

    Alter 'get_sub_menu_template' to factor in differing template for each level, and cache template instances for each level to maintain efficiency

--- wagtailmenus/models/mixins.py
+++ wagtailmenus/models/mixins.py
@@ -7,13 +7,43 @@ from .. import app_settings
 class DefinesSubMenuTemplatesMixin:
     sub_menu_template_name = None  # set to use a specific default template
 
-    def get_sub_menu_template(self):
+    def get_sub_menu_template(self, level=1):
+        if not hasattr(self, '_sub_menu_templates_by_level'):
+            # This will be used to cache fetched template instance
+            self._sub_menu_templates_by_level = {}
+        elif level in self._sub_menu_templates_by_level:
+            # Return a cached template instance
+            return self._sub_menu_templates_by_level[level]
+        # Identify a fetch a new template instance for `level`
+        template_name = self.get_sub_menu_template_name_for_level(level)
+        if template_name:
+            t = get_template(template_name)
+        else:
+            t = select_template(self.get_sub_menu_template_names())
+        # Cache the template instance before returning
+        self._sub_menu_templates_by_level[level] = t
+        return t
+
+    def get_sub_menu_template_name_for_level(self, level):
+        # Prefer the 'sub_menu_template_name' passed as an option value,
+        # followed by a 'sub_menu_template_name' attribute on the menu class
         template_name = self._option_vals.sub_menu_template_name or \
             self.sub_menu_template_name
-
         if template_name:
-            return get_template(template_name)
-        return select_template(self.get_sub_menu_template_names())
+            return template_name
+
+        # Then prefer 'sub_menu_template_names' passed as an option value,
+        # followed by default template names (if values have been specified via
+        # the relevant app settings)
+        template_names = self._option_vals.sub_menu_template_names or \
+            self.get_default_sub_menu_template_names()
+        if template_names:
+            # Use the sub_menu template specified for this exact level if
+            # there is one. Otherwise, use the last item in the list
+            try:
+                return template_names[level - 1]
+            except KeyError:
+                return template_names[-1]
 
     @cached_property
     def sub_menu_template(self):
