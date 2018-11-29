9878715c59fc7901625ee132b8781d6a0a38d00b
Andy Babic <ababic@rkh.co.uk>
2018-11-29 15:40:24 +0000

    - Rename a bunch of things for consistency - Get the argument validator form showing in the custom renderer view - Moved custom form fields to a separate module - Made 'current_url' a required argument for all views - Added a tonne of help text to fields to help with explain what view arguments are for

--- wagtailmenus/api/v1/urls.py
+++ wagtailmenus/api/v1/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^main/$', views.RenderMainMenuView.as_view()),
-    url(r'^flat/$', views.RenderFlatMenuView.as_view()),
-    url(r'^children/$', views.RenderChildrenMenuView.as_view()),
-    url(r'^section/$', views.RenderSectionMenuView.as_view()),
+    url(r'^main_menu/$', views.MainMenuGeneratorView.as_view()),
+    url(r'^flat_menu/$', views.FlatMenuGeneratorView.as_view()),
+    url(r'^children_menu/$', views.ChildrenMenuGeneratorView.as_view()),
+    url(r'^section_menu/$', views.SectionMenuGeneratorView.as_view()),
 ]
